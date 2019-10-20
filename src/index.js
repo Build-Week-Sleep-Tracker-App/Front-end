de0fbeb66cdfc345f0014026f13e30611bd0cdd9
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-20 16:40:10 +0100

    Created react app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SleepTracker from './SleepTracker';
+import './index.css';
+
+ReactDOM.render(<SleepTracker />, document.getElementById('root'));
