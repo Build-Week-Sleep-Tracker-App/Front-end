64eddd0453947d455d0b5b171ea3d7b6ea3fdff5
Dom <domeccleston@gmail.com>
2019-10-21 20:14:30 +0100

    added recharts

--- src/SleepTracker.js
+++ src/SleepTracker.js
@@ -3,12 +3,14 @@ import { Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import { PrivateRoute } from "./utils/index";
 import SleepEntryForm from "./components/SleepEntryForm";
+import SleepGraph from "./components/SleepGraph";
 
 export default function SleepTracker() {
   return (
     <div className="sleepTracker">
       <Route exact path="/" component={LoginPage} />
       <PrivateRoute path="/sleepentryform" component={SleepEntryForm} />
+      <PrivateRoute path="/sleepgraph" component={SleepGraph} />
     </div>
   );
 }
