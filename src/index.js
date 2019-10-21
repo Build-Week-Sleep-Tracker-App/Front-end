a512fd7a215e972778893de3f41fa96f76945041
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-21 10:34:25 +0100

    Refactored redux store to different file

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'thunk';
-import reducers from './reducers';
 import SleepTracker from './SleepTracker';
+import store from './store';
 import './index.css';
 
 
 ReactDOM.render(
-	<Provider store={
-		createStore(
-			// reducers
-			combineReducers({
-				login: reducers.loginReducer
-			}),
-			// local storage ?
-			// middleware
-			applyMiddleware(thunk),
-			// redux dev tools
-			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-		)
-	}>
+	<Provider store={store}>
 		<Router>
 			<SleepTracker />
 		</Router>
---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+const store = createStore(
+	// reducers
+	combineReducers({
+		login: reducers.loginReducer
+	}),
+	// local storage ?
+	// middleware
+	applyMiddleware(thunk),
+	// redux dev tools
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
