94c9ae927c59fde00439dde39eea04ff148be8d8
Dom <domeccleston@gmail.com>
2019-10-21 11:20:58 +0100

    created utils folder for axioswithauth and privateroute

--- src/SleepTracker.js
+++ src/SleepTracker.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Login from './components/Login'
 
 export default function SleepTracker() {
 	return (
 		<div className="sleepTracker">
-
+			<Login />
 		</div>
 	);
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { withFormik, Form, Field } from 'formik';
 
 export default function Login (props) {
 	return(
 		<div>
-			{props.children}
+			<h1>Login</h1>
+			 <Form>
+			 </Form>
 		</div>
 	)
 }
