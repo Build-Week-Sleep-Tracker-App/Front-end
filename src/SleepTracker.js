c82ae50c783080616f2da03b23ebc8fcb25ea96f
aapetsi <apetsiampiah@gmail.com>
2019-10-21 13:56:23 +0000

    Add sleep tracker form to dashboard

--- src/SleepTracker.js
+++ src/SleepTracker.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Login from './components/Login';
-import Register from './components/Register';
+import React from 'react'
+import Login from './components/Login'
+import Register from './components/Register'
+import SleepEntryForm from './components/SleepEntryForm'
 
 export default function SleepTracker() {
-	return (
-		<div className="sleepTracker">
-			<Login />
-			<Register />
-		</div>
-	);
+  return (
+    <div className="sleepTracker">
+      <Login />
+      <Register />
+      <SleepEntryForm />
+    </div>
+  )
 }
--- src/components/SleepEntryForm.js
+++ src/components/SleepEntryForm.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function SleepEntryForm (props) {
-	return(
-		<div>
-			<h1>Sleep Entry Form</h1>
-			{props.children}
-		</div>
-	)
+const SleepEntryForm = props => {
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+  return (
+    <div>
+      <h1>Sleep Entry Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Time you went to bed:</label>
+        <input type="time" placeholder="Time you went to bed" />
+        <br />
+
+        <label>Time you woke up:</label>
+        <input type="time" placeholder="Time you woke up" />
+        <br />
+
+        <label>
+          How tired were you at bedtime?
+          <input type="radio" name="tired" value="1" />1
+          <input type="radio" name="tired" value="2" />2
+          <input type="radio" name="tired" value="3" />3
+          <input type="radio" name="tired" value="4" />4
+        </label>
+        <br />
+
+        <label>
+          How tired were you when waking up
+          <input type="radio" name="wake" value="1" />1
+          <input type="radio" name="wake" value="2" />2
+          <input type="radio" name="wake" value="3" />3
+          <input type="radio" name="wake" value="4" />4
+        </label>
+        <br />
+
+        <label>
+          How tired were you during the day
+          <input type="radio" name="day" value="1" />1
+          <input type="radio" name="day" value="2" />2
+          <input type="radio" name="day" value="3" />3
+          <input type="radio" name="day" value="4" />4
+        </label>
+        <br />
+        <button>Add sleep entry</button>
+      </form>
+    </div>
+  )
 }
+
+export default SleepEntryForm
