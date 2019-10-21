a09fd0e5b7be4aa004f9bab07c8060b61ffc848a
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-21 13:57:39 +0100

    Added sleep entry form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import types from '../actions/actionTypes';
+import moment from 'moment';
+
 const initialLoginState = {
 	username: "",
 	password: ""
 }
 const loginReducer = (state = initialLoginState, action) => {
-	switch(action.type) {
+	switch (action.type) {
 		case types.LOGIN:
 			return state;
 		default:
@@ -12,6 +14,44 @@ const loginReducer = (state = initialLoginState, action) => {
 	}
 }
 
+const initialUserState = {};
+const userReducer = (state = initialUserState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
+
+const today = moment().format('YYYY/MM/DDTHH:mm');
+const initialSleepFormState = {
+	"userID": "",
+	"start": today,
+	"end": today,
+	"difference": "",
+	"bed_t_tracking": "",
+	"work_t_tracking": "",
+	//"avarage_rating": "",
+	"average_rating": "",
+};
+const sleepFormReducer = (state = initialSleepFormState, action) => {
+	switch (action.type) {
+		case types.ON_CHANGE_SLEEP_FORM:
+			return {
+				...state,
+				...action.payload
+			}
+		default:
+			return state;
+	}
+}
+
 export default {
-	loginReducer
+	initialLoginState,
+	loginReducer,
+
+	initialUserState,
+	userReducer,
+
+	initialSleepFormState,
+	sleepFormReducer,
 }
