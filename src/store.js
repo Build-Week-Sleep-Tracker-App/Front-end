0301db4b5611b49af837e7151b80c7e25947fb8b
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-21 13:54:59 +0100

    Added user and sleepForm slices of state | Fixed error from missing compose

--- src/store.js
+++ src/store.js
@@ -1,16 +1,20 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 const store = createStore(
 	// reducers
 	combineReducers({
-		login: reducers.loginReducer
+		login: reducers.loginReducer,
+		user: reducers.userReducer,
+		sleepForm: reducers.sleepFormReducer
 	}),
 	// local storage ?
-	// middleware
-	applyMiddleware(thunk),
-	// redux dev tools
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	compose(
+		// middleware
+		applyMiddleware(thunk),
+		// redux dev tools
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 );
 
 export default store;
