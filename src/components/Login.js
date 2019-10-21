b206c8f2fc5f07fa1c8f0e7f69ef46767f4c0ac2
Dom <domeccleston@gmail.com>
2019-10-21 11:31:45 +0100

    made basic login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import { withFormik, Form, Field } from 'formik';
 
-export default function Login (props) {
+function Login (props) {
 	return(
 		<div>
 			<h1>Login</h1>
 			 <Form>
+				 <Field type="text" name="username" placeholder="Username" />
+				 <Field type="password" name="password" placeholder="Password" />
+				 <button type="submit">Login</button>
 			 </Form>
 		</div>
 	)
 }
+
+const FormikLoginForm = withFormik({
+	mapPropsToValues({ username, password }) {
+	  return {
+		username: username || "",
+		password: password || ""
+	  };
+	},
+  
+	handleSubmit(values) {
+	  console.log(values);
+	}
+  })(Login);
+
+export default FormikLoginForm;
