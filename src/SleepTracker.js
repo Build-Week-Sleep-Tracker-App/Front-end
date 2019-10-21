805bf7cf0f5550a3479b4c57a87abbdc5b903930
Dom <domeccleston@gmail.com>
2019-10-21 11:38:21 +0100

    added basic formik register form

--- src/SleepTracker.js
+++ src/SleepTracker.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Login from './components/Login'
+import Login from './components/Login';
+import Register from './components/Register';
 
 export default function SleepTracker() {
 	return (
 		<div className="sleepTracker">
 			<Login />
+			<Register />
 		</div>
 	);
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { withFormik, Form, Field } from 'formik';
 
-export default function Register (props) {
+function Register (props) {
 	return(
 		<div>
-			{props.children}
+			<h1>Register</h1>
+			<Form>
+				<Field type="text" name="username" placeholder ="username"/>
+				<Field type="password" name="password" placeholder="password" />
+				<Field type="date" name="birthdate" placeholder="birthdate" />
+				<button type="submit">Register</button>
+			</Form>
 		</div>
 	)
 }
+
+
+const FormikRegisterForm = withFormik({
+	mapPropsToValues({ username, birthdate, password }) {
+	  return {
+		username: username || "",
+		birthdate: birthdate || "",
+		password: password || ""
+	  };
+	},
+  
+	handleSubmit(values) {
+	  console.log(values);
+	}
+  })(Register);
+
+export default FormikRegisterForm;
