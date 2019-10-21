228251bad7f623aedd823174f697e1e180b27e07
Dom <domeccleston@gmail.com>
2019-10-21 16:02:50 +0100

    confirm password should be in register component

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,11 +9,9 @@ function Login({ errors, touched}) {
       <Form>
         <Field type="text" name="username" placeholder="Username" />
         <Field type="password" name="password" placeholder="Password" />
-		<Field type="password" name="confirmPassword" placeholder="Confirm Password" />
         <button type="submit">Login</button>
 		{touched.username && errors.username && <p>{errors.username}</p>}
 		{touched.password && errors.password && <p>{errors.password}</p>}
-		{touched.confirmPassword && errors.confirmPassword && <p>{errors.confirmPassword}</p>}
       </Form>
     </div>
   );
@@ -24,14 +22,12 @@ const FormikLoginForm = withFormik({
     return {
       username: username || "",
 	  password: password || "",
-	  confirmPassword: confirmPassword || "",
     };
   },
 
   validationSchema: Yup.object().shape({
 	  username: Yup.string().required("Please enter your username"),
 	  password: Yup.string().required("Please enter your password"),
-	  confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match')
   }),	
 
   handleSubmit(values, { props }) {
