f92b558199ee490f136015c1be3cf9155ddfd8d6
aapetsi <apetsiampiah@gmail.com>
2019-10-22 05:50:13 +0000

    Conditionally render login or logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,16 @@ import { LinkContainer } from 'react-router-bootstrap'
 import { NavLink } from 'react-router-dom'
 import logo from './logo.svg'
 
+// check if user is authenticated
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token')
+  return token ? true : false
+}
+console.log(isAuthenticated())
+
 const Header = () => (
   <div>
-    <Navbar bg="dark" variant="dark" expand="md">
+    <Navbar bg="dark" variant="dark" expand="sm">
       <Navbar.Brand>
         <NavLink to="/">
           <img
@@ -21,15 +28,18 @@ const Header = () => (
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <LinkContainer to="/">
-          <Button variant="link">Login</Button>
-        </LinkContainer>
         <LinkContainer to="/sleepentryform">
           <Button variant="link">Sleep Entry Form</Button>
         </LinkContainer>
-        <LinkContainer to="/logout">
-          <Button variant="link">Logout</Button>
-        </LinkContainer>
+        {isAuthenticated() ? (
+          <LinkContainer to="/logout">
+            <Button variant="link">Logout</Button>
+          </LinkContainer>
+        ) : (
+          <LinkContainer to="/">
+            <Button variant="link">Login</Button>
+          </LinkContainer>
+        )}
       </Navbar.Collapse>
     </Navbar>
   </div>
