05860d88d09c666b3209b3dd527259a9e3b597b6
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-23 22:57:34 +0100

    Refactored bootstrap table to be cards with overlays instead

--- src/components/SleepEntryList.js
+++ src/components/SleepEntryList.js
@@ -2,53 +2,70 @@ import React from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
-import { Table } from 'react-bootstrap';
 
-const SleepEntryList = ({ user, startEditSleepEntry, deleteSleepEntry }) => {
-  const onEditEntry = entry => e => {
-    startEditSleepEntry(entry);
-  };
-  const onDeleteEntry = id => e => {
-    deleteSleepEntry(id);
-  };
-  return (
-    <section className="sleepEntryList">
+const SleepEntryList = ({ user, isEditingEntry, startEditSleepEntry, deleteSleepEntry }) => {
+	const onEditEntry = entry => e => {
+		// todo: auto scroll up to form
+		startEditSleepEntry(entry);
+	};
+	const onDeleteEntry = id => e => {
+		deleteSleepEntry(id);
+	};
+	const scoreToIcon = (score) => {
+		const icons = ['icon-sad', 'icon-meh', 'icon-happy', 'icon-very-happy'];
+		return <i className={icons[score - 1]}></i>
+	}
+	const toggleOverlay = e => {
+		let target = e.target;
+		let targetClassList = Array.from(target.classList);
+		if(targetClassList.includes('sleepEntry')) {
+			targetClassList.toggle('clicked');
+		} else {
+			while(!targetClassList.includes('sleepEntry')) {
+				target = target.parentElement;
+				targetClassList = Array.from(target.classList);
+			}
+			target.classList.toggle('clicked');
+		}
+	}
+	return (
+		<section className="sleepEntryListSection">
 			<h1 className="text">Your sleep sessions</h1>
-      <Table striped bordered hover responsive>
-        <thead>
-          <tr>
-            <th>Start</th>
-            <th>End</th>
-            <th>Before sleep</th>
-            <th>After wake up</th>
-            <th>During the day</th>
-            <th>Edit entry</th>
-            <th>Delete entry</th>
-          </tr>
-        </thead>
-        <tbody>
-          {user.sleepData.map(entry => (
-            <tr key={entry.id}>
-              <td>{moment(entry.start).format('HH:mm DD/MM/YYYY')}</td>
-              <td>{moment(entry.end).format('HH:mm DD/MM/YYYY')}</td>
-              <td>{entry.bed_t_tracking}</td>
-              <td>{entry.work_t_tracking}</td>
-              <td>{entry.average_rating}</td>
-              <td>
-                <button onClick={onEditEntry(entry)}><i className="icon-edit"></i></button>
-              </td>
-              <td>
-                <button onClick={onDeleteEntry(entry.id)}><i className="icon-delete"></i></button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </section>
-  );
+			<div className="sleepEntryList">
+				{user.sleepData.length ? user.sleepData.map(entry => (
+					<div key={entry.id} className={`sleepEntry${isEditingEntry === entry.id ? ' editing' : ''}`} onClick={toggleOverlay}>
+						<div className="entryInfo">
+							<span>Started at</span>
+							<span>{moment(entry.start).format('HH:mm DD/MM/YYYY')}</span>
+						</div>
+						<div className="entryInfo">
+							<span>Ended at</span>
+							<span>{moment(entry.end).format('HH:mm DD/MM/YYYY')}</span>
+						</div>
+						<div className="entryInfo">
+							<span>Mood before sleep</span>
+							<span>{scoreToIcon(entry.bed_t_tracking)}</span>
+						</div>
+						<div className="entryInfo">
+							<span>Mood after waking up</span>
+							<span>{scoreToIcon(entry.work_t_tracking)}</span>
+						</div>
+						<div className="entryInfo">
+							<span>Mood during day</span>
+							<span>{scoreToIcon(entry.average_rating)}</span>
+						</div>
+						<div className="overlay">
+							<button onClick={onEditEntry(entry)}><i className="icon-edit"></i></button>
+							<button onClick={onDeleteEntry(entry.id)}><i className="icon-delete"></i></button>
+						</div>
+					</div>
+				)) : <p>Not enough user data</p>}
+			</div>
+		</section>
+	);
 };
 
 export default connect(
-  state => state,
-  actionCreators
+	state => state,
+	actionCreators
 )(SleepEntryList);
