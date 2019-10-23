afb6001f65bfce28531224a2495fb76f51a9dd14
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-23 22:55:57 +0100

    Added basic footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,14 @@ const Footer = props => {
 	return (
 		<footer>
 			<div className="inner">
-
+				<div className="logo">
+					<img src={logo} alt="Logo" />
+					<span>Sleep Tracker App</span>
+				</div>
+				<div className="moreInfo">
+					<a href="https://awesome-sleep-tracker.netlify.com/" target="_blank" rel="noopener noreferrer">Marketing</a>
+					<a href="https://build-week-sleep-tracker-app.github.io/Marketing-page-2/" target="_blank" rel="noopener noreferrer">Marketing 2</a>
+				</div>
 			</div>
 		</footer>
 	);
