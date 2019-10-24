60f369f196a274bba6612852f819a596d286002c
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-24 15:58:18 +0100

    Added total bed hours and checks for data before showing recommendation

--- src/components/SleepRecommendation.js
+++ src/components/SleepRecommendation.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
 
 const SleepRecommendation = ({ user }) => {
+	if(user.sleepData.length < 1) {
+		return null;
+	}
 	const totalTimeInBed = user.sleepData.reduce((hours, entry) => hours + entry.hours, 0);
 	let optimalMoodAfterGivenHours = user.sleepData.reduce((moodAverages, entry) => {
 		return [...moodAverages, {
@@ -13,13 +16,10 @@ const SleepRecommendation = ({ user }) => {
 		}]
 	}, []);
 	optimalMoodAfterGivenHours = optimalMoodAfterGivenHours.reduce((entryMax, entry) => (entry.average_mood > entryMax.average_mood ? entry : entryMax), { average_mood: 0, moodPerHour: 0 })
-	console.log({
-		totalTimeInBed,
-		optimalMoodAfterGivenHours
-	})
 	return (
 		<div className="recommendation">
 			<h1 className="text">Our recommendation</h1>
+			<p className="alignWithHeader">You have a total of {totalTimeInBed} hours in bed!</p>
 			<p className="alignWithHeader">Your mood score tends to be highest when you sleep around {optimalMoodAfterGivenHours.hours} hours!</p>
 		</div>
 	);
